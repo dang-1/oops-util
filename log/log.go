30386adea74c8cda9eba8185243aa0967a0cbc3d
dang <dang-1@users.noreply.github.com>
2021-03-22 11:09:00 +0800

    Create log.go

---
+++ log/log.go
@@ -0,0 +1,27 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func init() {
+	fmt.Println("set log")
+	log.Printf("init log config")
+	// logFile := "test.log"
+	hostname, err := os.Hostname()
+	fmt.Println(hostname, err)
+	if err != nil {
+		log.Fatal("get host name error")
+	}
+	logFile := fmt.Sprintf("/data/log/%s.log", hostname)
+	logF, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// logF, err := os.OpenFile(logFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	mv := io.MultiWriter(os.Stdout, logF)
+	log.SetOutput(mv) //写入控制台和文件
+	// log.SetOutput(logF)
+	log.SetPrefix(hostname)
+	log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
+}
